e7bf63cf33a6c888356d1f57321abb2f47a7c059
Irsten <irsten3@gmail.com>
2023-06-16 13:21:52 +0200

    Add expenses list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Expenses from './components/Expenses/Expenses';
 import './App.css';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const expenses = [
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: 'Car', amount: 3123.67, date: new Date(2021, 2, 28) },
+    { title: 'Burger', amount: 24.67, date: new Date(2021, 2, 1) },
+    { title: 'Books', amount: 200.67, date: new Date(2021, 1, 13) },
+  ];
+
+  return <Expenses items={expenses} />;
+};
 
 export default App;
